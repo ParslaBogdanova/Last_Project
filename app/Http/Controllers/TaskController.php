a5ae3794f00fcd77d0b8d4aac775501f48128b0c
Your Name <you@example.com>
2024-11-21 19:58:43 +0200

    Every fuction leands to tasks.index

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function store(Request $request)
+    {
+\Log::info($request->all());
+
+        $request->validate([
+            'description' => 'required|string',
+        ]);
+
+        Task::create([
+            'description' => $request->description,
+            'is_completed' => $request->has('is_completed'),
+        ]);
+        return redirect()->route('tasks.index');
+
+    }
+
+    public function show(Task $task)
+    {
+        return view('tasks.show', ['task' => $task]);
+    }
+
+    public function edit($id)
+    {
+        $task = Task::findOrFile($id);
+        return view('tasks.edit', ['task' => $task]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'description' => 'request',
+        ]);
+
+        $task = Task::findOrFail($id);
+        $task->update([
+            'description' => $request->input('description'),
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
