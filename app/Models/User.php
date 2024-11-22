743c89ae771251cdfb7e029355ff7a0c7fc009ff
Your Name <you@example.com>
2024-11-22 10:55:16 +0200

    New atributres and one-to-many relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tools(){
+        return $this->hasMany(UserTool::class);
+    }
 }
---
+++ app/Models/UserTool.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTool extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'tool_name',
+        'email',
+        'password',
+        'display_name',
+    ];
+
+    // Relationship with the User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
