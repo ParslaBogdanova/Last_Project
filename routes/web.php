65b28356156afb62b1a0c3616b8bbaf2ff11923a
Your Name <you@example.com>
2024-12-12 17:12:06 +0200

    add middleware for task, so other user doesnt see other users task.

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::middleware('auth')->group(function () {
 Route::resource('tasks', TaskController::class);
+});
+
 Route::get('/calendar/{month?}/{year?}', [DayController::class, 'index'])->name('calendar.index');
 Route::get('/calendar/day/{dayId}', [DayController::class, 'show'])->name('calendar.day');
 Route::get('/calendar/day/{date}/schedules', [ScheduleController::class, 'getSchedulesForDay']);
-Route::post('/calendar/day/{date}/schedule/store', [ScheduleController::class, 'store'])->name('schedule.store'); // Updated route
+Route::post('/calendar/day/{date}/schedule/store', [ScheduleController::class, 'store'])->name('schedule.store');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('tasks', TaskController::class);
 });
 
 require __DIR__.'/auth.php';
