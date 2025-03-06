e65e28510106daa9b27fed6bb1d30cad2f5e9625
Your Name <you@example.com>
2025-03-06 18:49:10 +0200

    add zoomMeeting

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -49,7 +49,7 @@ class CalendarController extends Controller
 
     public function show($month, $year, $day_id)
     {
-        $day = Day::with(['schedules', 'blockedDays'])->findOrFail($day_id);
+        $day = Day::with(['schedules', 'blockedDays', 'zoomMeetings'])->findOrFail($day_id);
 
         if ($day->calendar->user_id !== Auth::id() || $day->calendar->month != $month || $day->calendar->year != $year) {
             abort(403, 'Unauthorized action.');
@@ -60,6 +60,7 @@ class CalendarController extends Controller
             'day' => $day,
             'schedules' => $day->schedules,
             'blockedDays' => $day->blockedDays,
+            'zoomMeetings' => $day->zoomMeetings,
             'month' => $month,
             'year' => $year,
             'users' => $users,
