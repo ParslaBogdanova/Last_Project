2c7e7d82670bad6dd977ec2b66a73d8311d1c928
Your Name <you@example.com>
2025-04-03 23:57:09 +0300

    Two new models

---
+++ app/Models/Notification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'zoom_meetings_id',
+        'message',
+        'is_read',
+    ];
+
+    public function zoomMeeting(){
+        return $this->belongsTo(ZoomMeeting::class, 'zoom_meetings_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/ReminderZoomMeeting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ReminderZoomMeeting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'zoom_meetings_id',
+        'seen',
+    ];
+
+    public function zoomMeeting(){
+        return $this->belongsTo(ZoomMeeting::class, 'zoom_meetings_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
