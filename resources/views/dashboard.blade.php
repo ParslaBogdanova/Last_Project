187ab84a4ee19dbc4b325cb7f1b18c482719bbfa
Your Name <you@example.com>
2025-04-29 02:56:48 +0300

    just update it style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,23 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div
+                style="
+                background-color: #f9f1e9;
+                border: 1px solid #d5bdaf;
+                border-radius: 8px;
+                box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+                font-family: 'Cookie', cursive;
+                color: #5e503f;
+                overflow: hidden;
+                padding: 2rem;
+                text-align: center;
+            ">
+                <h2 style="font-size: 20px;">
                     {{ __("You're logged in!") }}
-                </div>
+                </h2>
             </div>
         </div>
     </div>
+
 </x-app-layout>
